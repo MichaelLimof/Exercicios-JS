d8945bd2e161f0432cfd8667b7e254318dd672dd
unknown <michael.limof@gmail.com>
2020-01-15 23:45:00 -0300

    continuos deploy

--- Seção 2/ParamPadrao.js
+++ Seção 2/ParamPadrao.js
@@ -20,3 +20,11 @@ function soma2(a, b, c) {
 }
 
 console.log(soma2(1, 2, 3), soma2(2), soma2(2, 2), soma2(0, 0, 0))
+
+
+
+//valor padrão do Es2015
+function soma3(a = 1, b = 1, c = 1) {
+    return a + b + c
+}
+console.log(soma3(1, 2, 3), soma3(2), soma3(2, 2), soma3(0, 0, 0))
